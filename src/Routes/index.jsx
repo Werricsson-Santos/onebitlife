751c216e32471d4bd446090c29f444edbd2dd83f
Werricsson <werricsson.santos@hotmail.com>
2024-01-14 11:35:38 -0300

    Renderização condicional de página, tempo de vida dinâmico e correção dos nomes das variáveis e da coluna 'appStartDate'

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 import AllPages from "./allPages";
 import HomePage from "./HomePage";
+import ChangeNavigationService from "../Services/ChangeNavigationService";
 
 export default function Routes() {
     const [showHome, setShowHome] = useState("false");
+    ChangeNavigationService.checkShowHome(1)
+        .then((showHome) => {
+            setShowHome(showHome.showHome);
+        })
+        .catch((err) => console.log(err));
+        
 
     return <>{showHome === "true" ? <HomePage /> : <AllPages />}</>;
 }
