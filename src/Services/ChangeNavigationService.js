380fc3c398587b1e59524ad4931f14af0b10135c
Werricsson <werricsson.santos@hotmail.com>
2024-01-14 10:29:25 -0300

    Criando o serviço de GET

--- src/Services/ChangeNavigationService.js
+++ src/Services/ChangeNavigationService.js
@@ -30,5 +30,21 @@ const setShowHome = (obj) => {
     });
 };
 
-export default { setShowHome };
+const checkShowHome = (id) => {
+    return new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                "SELECT * FROM change_navigation WHERE id=?;",
+                [id],
+                (_, {rows}) => {
+                    if (rows.length > 0) resolve(rows._array[0]);
+                    else reject("Obj not found: id=" + id);
+                },
+                (_, error) => reject(error)
+            );
+        });
+    });
+};
+
+export default { setShowHome, checkShowHome };
 
